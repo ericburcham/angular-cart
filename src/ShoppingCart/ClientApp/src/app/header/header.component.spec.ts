27a2aee70c1f833aebc27922fad6d41e64c6d900
Eric Burcham <eburcham@eprod.com>
2022-04-01 12:41:17 -0500

    HeaderComponent.should display user name from UserDataService is failing

--- src/ShoppingCart/ClientApp/src/app/header/header.component.spec.ts
+++ src/ShoppingCart/ClientApp/src/app/header/header.component.spec.ts
@@ -1,12 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserService } from '../services/user.service';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let userServiceStub: Partial<UserService>;
 
   beforeEach(async () => {
+    // Stub UserService to provide the test username.
+    userServiceStub = {
+      username: 'Boba Fett'
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      providers: [{provide: UserService, useValue: userServiceStub}]
+    });
+
     await TestBed.configureTestingModule({
       declarations: [ HeaderComponent ]
     })
@@ -31,7 +43,8 @@ describe('HeaderComponent', () => {
   });
 
   it('should display user name from UserDataService', () =>{
-    
+    const headerElement = fixture.debugElement.nativeElement;
+    expect(headerElement.querySelector('.username').textContent).toBe('Boba Fett');
   });
 
   // -----------
