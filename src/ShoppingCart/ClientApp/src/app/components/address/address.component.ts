3462699469a84dd62872fa31e93ba202fe3e4b88
Ellis <REllis1@eprod.com>
2022-04-01 17:51:56 -0500

    Layout the Address Component

--- src/ShoppingCart/ClientApp/src/app/components/address/address.component.ts
+++ src/ShoppingCart/ClientApp/src/app/components/address/address.component.ts
@@ -3,21 +3,41 @@ import { ApiService } from 'src/app/services/api/api.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { ShoppingCartService } from '../shopping-cart/shopping-cart.service';
 
+class Address { 
+  addressee = '';
+  address1 = '';
+  address2 = '';
+  city = '';
+  state = '';
+  zip = '';
+}
+
 @Component({
   selector: 'address',
+  templateUrl: './address.component.html',
   styleUrls: ['./address.component.css'],
-  template: `
-    <strong>{{addressType}} address:</strong>
-  `,
 })
-
 export class AddressComponent implements OnInit {
-  @Input('addressType') addressType: string = '';
+
+  @Input('addressType') 
+  addressType: string = '';
+
+  address: Address;
+
   constructor(
     public user: UserService,
     public shoppingCart: ShoppingCartService,
     public api: ApiService,
-  ) {}
+  ) {
+    // this.address = user[`${this.addressType}Address`];
+    this.address = new Address();
+    this.address.address1 = "1234 Street Name"
+    this.address.address2 = "Suite 100";
+    this.address.city = "Houston";
+    this.address.state = "TX";
+    this.address.zip = "77044";
+  }
 
   ngOnInit(): void {}
+
 }
