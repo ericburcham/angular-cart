287b4a974928bc597c04910db8b8ebff3b455a4d
Eric Burcham <eburcham@eprod.com>
2022-04-01 13:45:06 -0500

    add the apply-discount-component stubs

---
+++ src/ShoppingCart/ClientApp/src/app/apply-discount/apply-discount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-discount',
+  templateUrl: './apply-discount.component.html',
+  styleUrls: ['./apply-discount.component.css']
+})
+export class ApplyDiscountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
