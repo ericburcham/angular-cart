8c260285cd37649c178d80c05b6c8efc8da1ed2c
Ellis <REllis1@eprod.com>
2022-04-04 13:43:25 -0500

    Add debouncing to add-new-item search

--- src/ShoppingCart/ClientApp/src/app/components/new-cart-item/new-cart-item.component.ts
+++ src/ShoppingCart/ClientApp/src/app/components/new-cart-item/new-cart-item.component.ts
@@ -1,13 +1,15 @@
-import { Component, EventEmitter, Input, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, tap } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product/product.service';
-import { CartItem, OrderSummary, Product } from '../types';
+import { CartItem, Product } from '../types';
 
 @Component({
   selector: 'new-cart-item',
   templateUrl: 'new-cart-item.component.html'
 })
 
-export class NewCartItemComponent {
+export class NewCartItemComponent implements AfterViewInit {
 
   selectedProduct?: Partial<CartItem>;
   suggestions: Product[] = [];
@@ -15,27 +17,42 @@ export class NewCartItemComponent {
   @Input('cartItems') cartItems: CartItem[] = [];
   @Output('onAdd') onAdd = new EventEmitter();
 
+  @ViewChild('input') input: any;
   @ViewChild('suggestionList') suggestionList: any;
-
+  
   constructor(
     public productService: ProductService,
   ) {}
 
-  ngOnChanges(changes: SimpleChanges) {
-  }
-
-  handleKey(e: any) {
-    e.stopPropagation();
-    const searchString = e.target.value;
-    this.productService.getSuggestions(searchString)
-      .subscribe((r: any) => {
-        if (r.ok) {
-          this.suggestions = r.data;
-        }
-      });
+  ngAfterViewInit() {
+    // server-side search
+    fromEvent(this.input.nativeElement,'keyup')
+      .pipe(
+          filter(Boolean),
+          debounceTime(200),
+          distinctUntilChanged(),
+          tap((e: any) => {
+            const searchString = this.input.nativeElement.value;
+            if (!searchString) {
+              this.suggestions = [];
+              return;
+            }
+            if (this.selectedProduct && this.selectedProduct.sku === searchString) {
+              return;
+            }
+            this.productService.getSuggestions(searchString)
+              .subscribe((r: any) => {
+                if (r.ok) {
+                  this.suggestions = r.data;
+                }
+              });
+          })
+      )
+      .subscribe();
   }
 
   handleSelect(e: any) {
+    e.stopPropagation();
     const selectedSku = e.target.value;
     this.selectedProduct = this.suggestions.find(p => p.sku === selectedSku);
   }
