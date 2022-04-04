3fdce23bc44c7f134b565a0e5e33bc7b54e7fc96
Ellis <REllis1@eprod.com>
2022-04-04 10:23:38 -0500

    Cleanup calculateOrderSummary()

--- src/ShoppingCart/ClientApp/src/app/components/order-summary/calculateOrderSummary.ts
+++ src/ShoppingCart/ClientApp/src/app/components/order-summary/calculateOrderSummary.ts
@@ -1,14 +1,9 @@
 import { CartItem, OrderSummary } from '../types';
 
 export function calculateOrderSummary(items: CartItem[]) {
-  const summary = items.reduce((s, x) => {
-    const summary = {
-      subTotal: (s.subTotal || 0) + x.subTotal,
-      discount: (s.discount || 0) + x.discount,
-      total: (s.total || 0) + x.total,
-    };
-
-    return summary;
-  }, {} as OrderSummary);
-  return summary;
+  return items.reduce((s, item) => ({
+      subTotal: (s.subTotal || 0) + item.subTotal,
+      discount: (s.discount || 0) + item.discount,
+      total: (s.total || 0) + item.total,
+  }), {} as OrderSummary);
 }
