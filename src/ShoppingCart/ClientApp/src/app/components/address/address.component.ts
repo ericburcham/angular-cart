c9564721e08605eebef4bf04a6ea59c9dff16d22
Eric Burcham <eburcham@eprod.com>
2022-04-05 11:15:15 -0500

    added header component spec

--- src/ShoppingCart/ClientApp/src/app/components/address/address.component.ts
+++ src/ShoppingCart/ClientApp/src/app/components/address/address.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'address',
   templateUrl: './address.component.html',
-  styleUrls: ['./address.component.css'],
 })
 export class AddressComponent {
   @Input('address') address: any = {};
