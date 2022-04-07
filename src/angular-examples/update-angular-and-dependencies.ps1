d41ecd14e09b514ae2750dd4b2030cfcdbdc199d
Eric Burcham <Eric Burcham>
2022-04-07 01:14:16 -0500

    update ps1 utilities

--- src/angular-examples/update-angular-and-dependencies.ps1
+++ src/angular-examples/update-angular-and-dependencies.ps1
@@ -15,15 +15,15 @@ foreach ($subdirectory in $subdirectories) {
     Write-Host "# --------------------------------------------------------------------------" -ForegroundColor Green;
     
     # Remove package-lock so we can rebuild it from scratch.
-    Remove-Item .\package-lock.json -ErrorAction Inquire;
+    Remove-Item .\package-lock.json -ErrorAction SilentlyContinue;
 
     # Update angular CLI
-    Remove-Item .\node_modules\ -ErrorAction Inquire -Force -Recurse;
-    npm uninstall --save-dev @angular/cli;
-    npm install --save-dev @angular/cli@latest;
+    Remove-Item .\node_modules\ -ErrorAction SilentlyContinue -Force -Recurse;
+    npm uninstall --force --save-dev @angular/cli;
+    npm install --force --save-dev @angular/cli@latest;
 
     # Update package dependencies
-    npm update --save;
+    npm update --force --save;
 
     # Fix vulnerabilities
     npm audit fix;
