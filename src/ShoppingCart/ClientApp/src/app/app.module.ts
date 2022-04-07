7e429bd146146b4dad41ff3ced9a109a6d0ed532
Eric Burcham <Eric Burcham>
2022-04-07 07:04:04 -0500

    finished the shopping cart module

--- src/ShoppingCart/ClientApp/src/app/app.module.ts
+++ src/ShoppingCart/ClientApp/src/app/app.module.ts
@@ -3,28 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AddressComponent } from './shared/components/address/address.component';
 import { AppComponent } from './app.component';
-import { CartItemsComponent } from './modules/shopping-cart/components/cart-items/cart-items.component';
-import { DealSelectorComponent } from './modules/shopping-cart/components/deal-selector/deal-selector.component';
-import { CartHeaderComponent } from './modules/shopping-cart/components/cart-header/cart-header.component';
-import { NewCartItemComponent } from './modules/shopping-cart/components/cart-item/cart-item.component';
-import { OrderSummaryComponent } from './modules/shopping-cart/components/order-summary/order-summary.component';
-import { ShoppingCartComponet } from './modules/shopping-cart/components/shopping-cart/shopping-cart.component';
 import { ShoppingCartModule } from './modules/shopping-cart/shopping-cart.module';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [
-    AddressComponent,
-    AppComponent,
-    CartHeaderComponent,
-    CartItemsComponent,
-    DealSelectorComponent,
-    OrderSummaryComponent,
-    NewCartItemComponent,
-    ShoppingCartComponet
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     FormsModule,
--- src/ShoppingCart/ClientApp/src/app/modules/shopping-cart/shopping-cart.module.ts
+++ src/ShoppingCart/ClientApp/src/app/modules/shopping-cart/shopping-cart.module.ts
@@ -1,8 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { AddressComponent } from 'src/app/shared/components/address/address.component';
+import { CartHeaderComponent } from './components/cart-header/cart-header.component';
+import { CartItemsComponent } from './components/cart-items/cart-items.component';
+import { DealSelectorComponent } from './components/deal-selector/deal-selector.component';
+import { NewCartItemComponent } from './components/cart-item/cart-item.component';
+import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+import { ShoppingCartComponet } from './components/shopping-cart/shopping-cart.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddressComponent,
+    CartHeaderComponent,
+    CartItemsComponent,
+    DealSelectorComponent,
+    OrderSummaryComponent,
+    NewCartItemComponent,
+    ShoppingCartComponet,
+  ],
+  exports: [ShoppingCartComponet],
   imports: [CommonModule],
 })
 export class ShoppingCartModule {}
